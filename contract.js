bfd8839246c7d4604705136b4f5513ab2fecf7f4
Jefwillems <jefwillems100@gmail.com>
2017-09-04 15:50:49 +0200

    removed code

--- contract.js
+++ contract.js
@@ -1,15 +1,8 @@
-function printContract() {
-    return "Hello World!";
-}
-
 function _getTimestampForId(id) {
     return new Date().valueOf();
 }
 
 module.exports = {
-    print: function () {
-        return printContract();
-    },
     getTimestampForId: (id) => {
         return _getTimestampForId(id);
     }
