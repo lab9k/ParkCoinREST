c862f3f5a2c29e14a0b1ae56ef8f250aea4f7814
Jefwillems <jefwillems100@gmail.com>
2017-09-04 15:11:15 +0200

    added body-parser configuration in app

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
 
 app.get('/', function (req, res) {
     res.send('Hello world!!!!');
@@ -11,9 +17,10 @@ app.get('/:plate', (req, res) => {
 });
 
 app.post('/new', (req, res) => {
-    res.send(req);
+    let plate = req.body.plate;
+    res.send(plate); 
 });
 
 app.listen(3000, function () {
     console.log('App listening on port 3000');
-}); 
+});
