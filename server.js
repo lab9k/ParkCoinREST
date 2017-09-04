5073eee7e4d16d1bc39e20b7da144dec8719e9c9
Michiel Derveeuw <mderveeuw@gmail.com>
2017-09-04 15:19:48 +0200

    Add mongodb dep, remove mongoose dep

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const config = require('./config.json');
+
+const url = 'mongodb://' + config.dburl;
 
 app.get('/', function (req, res) {
-    res.send('Hello world!!!!');
+    MongoClient.connect(url, function (err, db) {
+        console.log("Connected to parking db");
+        let collection = db.collection('licensePlates');
+        collection.find({}).toArray(function (err, docs) {
+            res.send(docs);
+            db.close();
+        });
+    });
 });
 
 app.get('/:plate', (req, res) => {
